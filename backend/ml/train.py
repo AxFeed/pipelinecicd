d8e1d2d6f46f8b57bc538794f88dcee2729678b0
AxFeed <lilian.lopez82290@gmail.com>
2026-04-07 16:57:56 +0200

    format ruff

--- backend/ml/train.py
+++ backend/ml/train.py
@@ -23,7 +23,9 @@ def train():
     logger.info("Chargement du dataset Iris...")
     iris = load_iris()
     X, y = iris.data, iris.target
-    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
+    X_train, X_test, y_train, y_test = train_test_split(
+        X, y, test_size=0.2, random_state=42
+    )
 
     mlflow.set_experiment("iris-classification")
     mlflow.sklearn.autolog(log_models=False)
